49153b22df10a75c367ec0793469fd4c63965a83
Silviu <silviu.chirvasa@est.fib.upc.edu>
2016-10-30 18:55:03 +0100

    Añadido separar palabras en clase Frase

--- src/edu/upc/fib/Frase.java
+++ src/edu/upc/fib/Frase.java
@@ -1,17 +1,36 @@
 package edu.upc.fib;
 
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Frase implements Comparable<Frase> {
     private Vector<Palabra> mFrase;
 
     public Frase(String frase) {
-        //String[] words = frase.split("([A-Za-z'ÁáÄäÀàÉéËëÈèÍíÏïÌìÓóÖöÒòÚúÜüÙùÑñÇçÑñ-])+");
-        String[] words = frase.split(" ");
-        mFrase = new Vector();
-        for (String s : words) {
-            mFrase.add(new Palabra(s));
+        Pattern pattern = Pattern.compile("([A-Za-z'ÁáÄäÀàÉéËëÈèÍíÏïÌìÓóÖöÒòÚúÜüÙùÑñÇçÑñ-])+|.");
+        Matcher matcher = pattern.matcher(frase);
+        mFrase = new Vector<>();
+        while (matcher.find()) {
+            mFrase.add(new Palabra(matcher.group()));
         }
+
+        System.out.println(matcher.groupCount());
+//        if (matcher.find()) {
+//            mFrase = new Vector<>();
+//            for (int i = 0; i < matcher.groupCount(); i++) {
+//                mFrase.add(new Palabra(matcher.group(i)));
+//            }
+//        }
+
+//        String[] words = frase.split("([A-Za-z'ÁáÄäÀàÉéËëÈèÍíÏïÌìÓóÖöÒòÚúÜüÙùÑñÇçÑñ-])+");
+//        String[] words = frase.split(".*");
+//        replaceFirst("")
+//        //String[] words = frase.split(" ");
+//        mFrase = new Vector();
+//        for (String s : words) {
+//            mFrase.add(new Palabra(s));
+//        }
     }
 
     /*public void print() {
